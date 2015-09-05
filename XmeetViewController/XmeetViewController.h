d7165922dfcfb62a03bc82d277e3055fe1049a1e
houkinglong <houkinglong@aliyun.com>
2015-09-05 19:10:28 +0800

    Initial Commit

---
+++ XmeetViewController/XmeetViewController.h
@@ -0,0 +1,13 @@
+//
+//  XmeetViewController.h
+//  XmeetViewController
+//
+//  Created by HouKinglong on 15/9/5.
+//  Copyright (c) 2015年 HouKinglong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XmeetViewController : NSObject
+
+@end
---
+++ XmeetViewController/XmeetViewController.m
@@ -0,0 +1,13 @@
+//
+//  XmeetViewController.m
+//  XmeetViewController
+//
+//  Created by HouKinglong on 15/9/5.
+//  Copyright (c) 2015年 HouKinglong. All rights reserved.
+//
+
+#import "XmeetViewController.h"
+
+@implementation XmeetViewController
+
+@end
